d0fb1c260406cb2207d88364af7a44dfe62b7767
Virvon <dan130205@gmail.com>
2023-06-29 17:44:25 +0300

    Update state machine

--- Assets/Sources/State Machine/Transitions/AttackTransition.cs
+++ Assets/Sources/State Machine/Transitions/AttackTransition.cs
@@ -2,23 +2,34 @@ using UnityEngine;
 
 namespace Clones.StateMachine
 {
-    public class AttackTransition : Transition
-    {
-        [SerializeField] private float _attackRadius;
+    public abstract class AttackTransition : Transition
+    { 
+        [SerializeField] private Player _player;
 
-        private readonly Collider[] _overlapColliders = new Collider[64];
+        private float _attackRadius => _player.AttackRadius;
+
+        protected readonly Collider[] _overlapColliders = new Collider[64];
 
         private void Update()
         {
-            int overlapCount = Physics.OverlapSphereNonAlloc(transform.position, _attackRadius, _overlapColliders);
+            if(IsTargetsEnter(Physics.OverlapSphereNonAlloc(transform.position, _attackRadius, _overlapColliders)))
+                NeedTransit = true;
+        }
 
+        protected virtual bool IsTargetsEnter(int overlapCount)
+        {
             for (var i = 0; i < overlapCount; i++)
             {
-                if (_overlapColliders[i].TryGetComponent(out Enemy enemy))
+                if (_overlapColliders[i].TryGetComponent(out IDamageble iDamageble))
                 {
-                    NeedTransit = true;
+                    if(IsRequiredTarget(iDamageble))
+                        return true;
                 }
             }
+
+            return false;
         }
+
+        protected abstract bool IsRequiredTarget(IDamageble iDamageble);
     }
 }
--- Assets/Sources/State Machine/Transitions/MiningTransition.cs
+++ Assets/Sources/State Machine/Transitions/MiningTransition.cs
@@ -2,31 +2,19 @@ using UnityEngine;
 
 namespace Clones.StateMachine
 {
-    public class MiningTransition : Transition
+    public class MiningTransition : AttackTransition
     {
-        [SerializeField] private float _miningRadius;
-
-        private readonly Collider[] _overlapColliders = new Collider[64];
-
-        private void Update()
+        protected override bool IsTargetsEnter(int overlapCount)
         {
-            int overlapCount = Physics.OverlapSphereNonAlloc(transform.position, _miningRadius, _overlapColliders);
-
-            for (var i = 0; i < overlapCount; i++)
+            for(var i = 0; i < overlapCount; i++)
             {
                 if (_overlapColliders[i].TryGetComponent(out Enemy enemy))
-                {
-                    return;
-                }
+                    return false;
             }
 
-            for (var i = 0; i < overlapCount; i++)
-            {
-                if (_overlapColliders[i].TryGetComponent(out MiningFacility miningFacility))
-                {
-                    NeedTransit = true;
-                }
-            }
+            return base.IsTargetsEnter(overlapCount);
         }
+
+        protected override bool IsRequiredTarget(IDamageble iDamageble) => iDamageble is MiningFacility ? true : false;
     }
 }
