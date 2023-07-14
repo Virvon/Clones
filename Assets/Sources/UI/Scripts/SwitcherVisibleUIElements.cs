0c6d96324d2e8a1f5ff960b537a27e5fd25a69ea
DrMax <tehnolog3ooo@yandex.ru>
2023-07-14 15:14:46 +0300

    Upgrade UI
    Add characters rangs

---
+++ Assets/Sources/UI/Scripts/SwitcherVisibleUIElements.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitcherVisibleUIElements : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _objectsActivate = new List<GameObject>();
+    [SerializeField] private List<GameObject> _objectsDeactivate = new List<GameObject>();
+    public void SwitchVisiblePanels()
+    {
+        foreach (var obj in _objectsActivate)
+            obj.SetActive(true);
+
+        foreach (var obj in _objectsDeactivate)
+            obj.SetActive(false);
+    }
+}
--- Assets/Sources/UI/Scripts/UIPanelsManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class UIPanelsManager : MonoBehaviour
-{
-    [SerializeField] private GameObject _panel;
-
-    public void SetVisiblePanel(bool isActive) => _panel.SetActive(isActive);
-}
