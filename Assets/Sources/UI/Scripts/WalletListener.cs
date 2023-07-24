a44eb9b7d921bf360a04d5f8d03916c5f0a33375
DrMax <tehnolog3ooo@yandex.ru>
2023-07-24 21:19:38 +0300

    Correct

--- Assets/Sources/UI/Scripts/WalletListener.cs
+++ Assets/Sources/UI/Scripts/WalletListener.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class WalletListener : MonoBehaviour
+[RequireComponent(typeof(NumberFormatter), (typeof(TextMeshPro)), typeof(Wallet))]
+public abstract class WalletListener
 {
     [SerializeField] private Wallet _wallet;
     [SerializeField] private TextMeshPro _text;
@@ -15,5 +16,14 @@ public class WalletListener : MonoBehaviour
         _wallet.ChangedCoinsCount.AddListener(UpdateText);
     }
 
-    private void UpdateText(int value) => _text.text = _numberFormatter.FormatNumberWithCommas(value);
+    private void UpdateText(int value)
+    {
+        _text.text = _numberFormatter.FormatNumberWithCommas(value);
+        SwitchAccessibility(value);
+    }
+
+    private void SwitchAccessibility(int value) 
+    {
+        
+    }
 }
