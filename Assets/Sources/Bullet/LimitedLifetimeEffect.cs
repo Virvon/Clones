49f580341974c11c8fa0e861e706f6dd6ad07c86
Virvon <dan130205@gmail.com>
2023-08-14 17:23:26 +0300

    Added lifetime to bullet effects

---
+++ Assets/Sources/Bullet/LimitedLifetimeEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LimitedLifetimeEffect : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime;
+
+    private void Start() => StartCoroutine(Timer());
+
+    private IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+
+        Destroy(gameObject);
+    }
+}
