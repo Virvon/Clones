9225f09e76dee3233eec7d683a81e4e06e2e6d54
DrMax <tehnolog3ooo@yandex.ru>
2023-10-24 21:18:26 +0300

    Added Epic Toon FX metas and add coin image

---
+++ Assets/Sources/UI/Scripts/UIFXActivator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFXActivator : MonoBehaviour
+{
+    [SerializeField] private GameObject _FXObjext;
+    [SerializeField] private float _duration;
+    //animation
+}
