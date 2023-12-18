74e2bb452ab69402cb1232c7390390bf9401627b
Virvon <dan130205@gmail.com>
2023-12-18 12:48:38 +0300

    Added base EnviromentSpawner

---
+++ Assets/Clones/Sources/GameLogic/WorldGenerator/EnviromentSpawner.cs
@@ -0,0 +1,40 @@
+﻿using Clones.Infrastructure;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Clones.GameLogic
+{
+    public abstract class EnviromentSpawner<TEnviroment> : MonoBehaviour where TEnviroment : Enum
+    {
+        [SerializeField] private Transform[] _spawnPoints;
+
+        private IPartsFactory _partsFactory;
+        private TEnviroment[] _enviromentsTypes;
+        private int _percentageFilled;
+
+        public void Init(IPartsFactory partsFactory, TEnviroment[] enviromentsTypes, int percentageFilled)
+        {
+            _partsFactory = partsFactory;
+            _enviromentsTypes = enviromentsTypes;
+            _percentageFilled = percentageFilled;
+
+            if (_enviromentsTypes.Length > 0)
+                Spawn();
+        }
+        protected abstract void CreateEnviromentPart(IPartsFactory partsFactory, TEnviroment enviromentType, Vector3 position);
+
+        private void Spawn()
+        {
+            foreach(Transform point in _spawnPoints)
+            {
+                if (CanSpawned())
+                    CreateEnviromentPart(_partsFactory, _enviromentsTypes[Random.Range(0, _enviromentsTypes.Length)], point.transform.position);
+            }
+        }
+
+        private bool CanSpawned() =>
+            Random.Range(0, 101) <= _percentageFilled;
+
+    }
+}
--- Assets/Clones/Sources/GameLogic/WorldGenerator/PreyResourcesSpawner.cs
+++ Assets/Clones/Sources/GameLogic/WorldGenerator/PreyResourcesSpawner.cs
@@ -4,41 +4,9 @@ using UnityEngine;
 
 namespace Clones.GameLogic
 {
-    public class PreyResourcesSpawner : MonoBehaviour
+    public class PreyResourcesSpawner : EnviromentSpawner<PreyResourceType>
     {
-        [SerializeField] private GameObject _spawnPointsWrapper;
-
-        private IPartsFactory _partsFactory;
-        private Point[] _spawnPoints;
-        private PreyResourceType[] _preyResourcesTemplates;
-        private int _percentageFilled;
-
-        public void Init(IPartsFactory partsFactory, PreyResourceType[] preyResourcesTemplates, int percentageFilled)
-        {
-            _partsFactory = partsFactory;
-            _preyResourcesTemplates = preyResourcesTemplates;
-            _percentageFilled = percentageFilled;
-
-            _spawnPoints = _spawnPointsWrapper.GetComponentsInChildren<Point>();
-
-            if(_preyResourcesTemplates.Length > 0)
-                Spawn();
-        }
-
-        private void Spawn()
-        {
-            for (var i = 0; i < _spawnPoints.Length; i++)
-            {
-                if (CanSpawned())
-                {
-                    PreyResourceType type = _preyResourcesTemplates[Random.Range(0, _preyResourcesTemplates.Length)];
-
-                    _partsFactory.CreatePreyResource(type, _spawnPoints[i].transform.position, Quaternion.identity, transform);
-                }
-            }
-        }
-
-        private bool CanSpawned() =>
-            Random.Range(0, 101) <= _percentageFilled;
+        protected override void CreateEnviromentPart(IPartsFactory partsFactory, PreyResourceType enviromentType, Vector3 position) => 
+            partsFactory.CreatePreyResource(enviromentType, position, Quaternion.identity, transform);
     }
 }
