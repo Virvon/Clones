ceb3dec0d30d5e954b59f7e7802e55ca74bc502d
Virvon <dan130205@gmail.com>
2024-02-19 21:04:14 +0300

    Added EducationHandler

---
+++ Assets/Clones/Sources/EducationLogic/EducationHandler/EducationHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Clones.EducationLogic
+{
+    public abstract class EducationHandler
+    {
+        public EducationHandler Successor { get; set; }
+        public abstract void Handle();
+    }
+
+    public class ShowQuestHandler : EducationHandler
+    {
+        public override void Handle()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
