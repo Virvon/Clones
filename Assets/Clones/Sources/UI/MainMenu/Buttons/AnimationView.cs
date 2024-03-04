0ab373d389c6742869fde3193607b49e66167b7e
Virvon <dan130205@gmail.com>
2024-03-04 17:53:33 +0300

    Added animations to settings

---
+++ Assets/Clones/Sources/UI/MainMenu/Buttons/AnimationView.cs
@@ -0,0 +1,25 @@
+﻿using Clones.Animation;
+using System;
+using UnityEngine;
+
+namespace Clones.UI
+{
+    public class AnimationView : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        private Action _callback;
+
+        public void Open() => 
+            _animator.SetBool(AnimationPath.UI.Bool.IsOpened, true);
+
+        public void Close(Action callback = null)
+        {
+            _callback = callback;
+            _animator.SetBool(AnimationPath.UI.Bool.IsOpened, false);
+        }
+
+        public void OnClosed() => 
+            _callback?.Invoke();
+    }
+}
---
+++ Assets/Clones/Sources/UI/MainMenu/Buttons/AnimationViewToggle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Clones.UI
+{
+    public class AnimationViewToggle : MonoBehaviour
+    {
+        [SerializeField] private AnimationView _animationView;
+
+        public void Open()
+        {
+            _animationView.gameObject.SetActive(true);
+            _animationView.Open();
+        }
+
+        public void Close() =>
+            _animationView.Close(() => _animationView.gameObject.SetActive(false));
+    }
+}
