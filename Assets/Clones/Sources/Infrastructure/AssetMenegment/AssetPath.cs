039b6e622eb12de1c4ffb585d50789b987c0fca4
Virvon <dan130205@gmail.com>
2024-04-18 12:06:14 +0300

    Refactored UI folder files position

--- Assets/Clones/Sources/Infrastructure/AssetMenegment/AssetPath.cs
+++ Assets/Clones/Sources/Infrastructure/AssetMenegment/AssetPath.cs
@@ -3,31 +3,31 @@
     public static class AssetPath
     {
         public const string Player = "Characters/Player/Prefabs/Character1.1";
-        public const string Hud = "UI/Game/Hud";
+        public const string Hud = "UI/Game/Prefabs/Hud";
         public const string VirtualCamera = "Camera/VirtualCamera";
-        public const string QuestView = "UI/Game/QuestView";
-        public const string ClonesCardsView = "UI/MainMenu/ScrollRect_Clones";
-        public const string WandsCardsView = "UI/MainMenu/ScrollRect_Wands";
-        public const string PlayButton = "UI/MainMenu/Button_Play";
-        public const string ClonesCardsShowButton = "UI/MainMenu/ClonesCardsShowButton";
-        public const string WandsCardsShowButton = "UI/MainMenu/WandsCardsShowButton";
-        public const string StatsView = "UI/MainMenu/StatsView";
-        public const string UpgradeView = "UI/MainMenu/UpgradeView";
-        public const string Joystick = "UI/Game/Control/Joystick";
-        public const string DesktopInput = "UI/Game/Control/DesktopInput";
+        public const string QuestView = "UI/Game/Prefabs/QuestView";
+        public const string ClonesCardsView = "UI/MainMenu/Prefabs/ScrollRect_Clones";
+        public const string WandsCardsView = "UI/MainMenu/Prefabs/ScrollRect_Wands";
+        public const string PlayButton = "UI/MainMenu/Prefabs/Button_Play";
+        public const string ClonesCardsShowButton = "UI/MainMenu/Prefabs/ClonesCardsShowButton";
+        public const string WandsCardsShowButton = "UI/MainMenu/Prefabs/WandsCardsShowButton";
+        public const string StatsView = "UI/MainMenu/Prefabs/StatsView";
+        public const string UpgradeView = "UI/MainMenu/Prefabs/UpgradeView";
+        public const string Joystick = "UI/Game/Prefabs/Control/Joystick";
+        public const string DesktopInput = "UI/Game/Prefabs/Control/DesktopInput";
         public const string GameMusic = "Audio/Prefabs/GameMusic";
         public const string FreezingScreen = "Camera/FreezingScreen";
-        public const string GameRevivleView = "UI/Game/RevivalView";
-        public const string GameOverView = "UI/Game/GameOverView";
-        public const string EducationOverView = "UI/Education/EducationOverView";
-        public const string ShowControlDialogPanel = "UI/Education/ShowControlDialogPanel";
-        public const string ShowFirstQuestDialogPanel = "UI/Education/ShowFirstQuestDialogPanel";
-        public const string ShowPreyResourcesDialogPanel = "UI/Education/ShowPreyResourcesDialogPanel";
-        public const string ShowSecondQuestDialogPanel = "UI/Education/ShowSecondQuestDialogPanel";
-        public const string SpawnFirstWaveDialogPanel = "UI/Education/SpawnFirstWaveDialogPanel";
-        public const string FrameFocus = "UI/Game/Focus";
+        public const string GameRevivleView = "UI/Game/Prefabs/RevivalView";
+        public const string GameOverView = "UI/Game/Prefabs/GameOverView";
+        public const string EducationOverView = "UI/Education/Prefabs/EducationOverView";
+        public const string ShowControlDialogPanel = "UI/Education/Prefabs/ShowControlDialogPanel";
+        public const string ShowFirstQuestDialogPanel = "UI/Education/Prefabs/ShowFirstQuestDialogPanel";
+        public const string ShowPreyResourcesDialogPanel = "UI/Education/Prefabs/ShowPreyResourcesDialogPanel";
+        public const string ShowSecondQuestDialogPanel = "UI/Education/Prefabs/ShowSecondQuestDialogPanel";
+        public const string SpawnFirstWaveDialogPanel = "UI/Education/Prefabs/SpawnFirstWaveDialogPanel";
+        public const string FrameFocus = "UI/Game/Prefabs/Focus";
         public const string AudioMixerGroup = "Audio/Mixers/Mixer";
         public const string EducationVirtualCamera = "Camera/EducationVirtualCamera";
-        public const string CloneModelPoint = "UI/MainMenu/CloneModelPoint";
+        public const string CloneModelPoint = "UI/MainMenu/Prefabs/CloneModelPoint";
     }
 }
